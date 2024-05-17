75efb24f7449ade8d749c2c1c3ee54f62d2a2d97
imlinux68 <imlinux68@gmail.com>
2024-05-17 08:11:14 +0300

    completed high of 3

--- find_high_of_3/high_of_3.c
+++ find_high_of_3/high_of_3.c
@@ -4,20 +4,20 @@
 
 int main() {
     int a,b,c;
-    printf("Enter the three numbers: ");
+    printf("Enter the three numbers: \n");
     scanf("%d%d%d", &a,&b,&c);
 
     if (a < b) {
         if (b < c) {
-            printf("Largest number is: %d",c);
+            printf("Largest number is: %d\n",c);
         }
 
         else if (b > c) {
-            printf("Largest number is: %d",b);
+            printf("Largest number is: %d\n",b);
         }
     }
     else {
-        printf("Largest number is: %d",a);
+        printf("Largest number is: %d\n",a);
     }
 
     return 0;
